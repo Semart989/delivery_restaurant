3348a54dac76fca80a53862045eee471b4d38d4f
Sergey <sergey.s1991@yandex.ru>
2021-12-09 20:18:24 +0300

    server fixed

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Navigation/>
-      <ControlledCarousel/>
+      {/* <ControlledCarousel/> */}
       DELIVERY RESTAURANT
     </div>
   );
