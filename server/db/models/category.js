9f8fa954a1bcbcde47b10755a7402b2d4cd165e2
lexu <alexey.uchkin@gmail.com>
2021-12-09 20:54:18 +0300

    database init

---
+++ server/db/models/category.js
@@ -0,0 +1,41 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Category extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ Dish, Dishes_Category }) {
+      Category.belongsToMany(Dish, { through: Dishes_Category, foreignKey: 'category_id', otherKey: 'dish_id'});
+      // define association here
+    }
+  };
+  Category.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    }
+  }, {
+    sequelize,
+    modelName: 'Category',
+  });
+  return Category;
+};
