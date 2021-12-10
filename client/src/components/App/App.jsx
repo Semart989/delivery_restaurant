7d16f18b11adebbfcc32f55dbe9bd0b9d80f1f84
Fedor A <fodor2994@mail.ru>
2021-12-10 19:43:47 +0300

    added custom hook for use local storage

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,13 +1,8 @@
 import './App.css';
 
-import ControlledCarousel from '../Carousel/Carousel'
-import ResponsiveAppBar from '../Nav/Nav'
-import DishCategory from '../DishCategory/DishCategory'
-import SFooter from '../SFooter/SFooter'
 import { Provider } from 'react-redux';
 import store from '../../redux/store';
 import LoginWrapper from '../LoginWrapper/LoginWrapper';
-
 
 import MainPage from '../MainPage/MainPage'
 import CartList from '../CartList/CartList'
