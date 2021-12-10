194ff2a3e72dd755d73393b5cb6739cf29b7e281
Fedor A <fodor2994@mail.ru>
2021-12-10 21:14:24 +0300

    added link to cart in navbar, and fixed routes app

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,12 +1,14 @@
-import './App.css';
-
 import { Provider } from 'react-redux';
-import store from '../../redux/store';
-import LoginWrapper from '../LoginWrapper/LoginWrapper';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import './App.css';
+import store from '../../redux/store';
+
+import LoginWrapper from '../LoginWrapper/LoginWrapper';
 import MainPage from '../MainPage/MainPage'
 import CartList from '../CartList/CartList'
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import ResponsiveAppBar from '../Nav/Nav'
+import SFooter from '../SFooter/SFooter'
 
 
 function App() {
@@ -18,21 +20,19 @@ function App() {
       <Provider store={store}>
         <LoginWrapper>
           <BrowserRouter>
+
+            <ResponsiveAppBar />
+
             <Switch>
-              <Route path="/" exact>
-                <MainPage />
-              </Route>
+              <Route path="/" exact component={MainPage} />
+              <Route path="/cart" exact component={CartList} />
             </Switch>
-            <Switch>
-              <Route path="/cart" exact>
-                <CartList />
-              </Route>
-            </Switch>
+
+            <SFooter />
+
           </BrowserRouter>
         </LoginWrapper>
       </Provider>
-
-
 
     </div>
   );
