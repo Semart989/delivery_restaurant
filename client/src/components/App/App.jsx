133707eb60b41f70cef7f999edcb849ca6279010
Konstantin Mamonov <stanze.von.saidenberg@yandex.ru>
2021-12-10 15:08:13 +0300

    Added components: AdminForm, AdminTaskList, TaskRoom, ViewContainer

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import ViewContainer from '../ViewContainer/ViewContainer';
 
 function App() {
   return (
     <div className="App">
       DELIVERY RESTAURANT
+      <ViewContainer />
     </div>
   );
 }
---
+++ client/src/components/TaskRoom/TaskRoom.jsx
@@ -0,0 +1,10 @@
+import styles from './TaskRoom.module.css';
+
+function TaskRoom(props) {
+  return (
+    <div className={styles.taskRoom}>
+    </div>
+  );
+}
+
+export default TaskRoom;
