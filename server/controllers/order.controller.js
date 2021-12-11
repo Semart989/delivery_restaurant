9888497bce4c3befa796940a48f67a77ba5b17dc
Artem <a.semenov989@gmail.com>
2021-12-11 16:31:00 +0300

    created order router

--- server/controllers/order.controller.js
+++ server/controllers/order.controller.js
@@ -2,7 +2,7 @@
 const { Order, Dish, Order_Dish } = require('../db/models');
 
 const order = {
-  user_id: 5,
+  user_id: 1,
   dishes: [{
     // order_id: 4,
     dish_id: 10,
@@ -16,25 +16,37 @@ const order = {
 
 const newOrder = async (req, res) => {
   // const { order } = req.body;
+  console.log(order.user_id);
 
   try {
     await Order.create({
       user_id: order.user_id,
     });
 
-    const orderId = await Order.findAll({ where: { user_id: order.user_id } });
+    const lastOrder = await Order.findAll({
+      // where: { user_id: order.user_id },
+      raw: true,
+      order: [['createdAt', 'DESC']],
+      limit: 1,
+    });
 
-    
-    await Order_Dish.create({
-      order_id: 123,
-      dish_id: 456,
-      quantity: 2,
+    console.log(lastOrder[0].id);
+
+    order.dishes.forEach(async (dish) => {
+      await Order_Dish.create({
+        order_id: lastOrder[0].id,
+        dish_id: dish.dish_id,
+        quantity: dish.quantity,
+      });
     });
 
     res.status(200).json({ success: true });
   } catch (error) {
     res.status(404).json({ error: 'error' });
+    console.log(error);
   }
 };
 
-module.exports = { newOrder };
+newOrder();
+
+// module.exports = { newOrder };
