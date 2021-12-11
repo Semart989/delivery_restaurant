82455a48469bad0de747482b379e05779d26ce86
Sergey <sergey.s1991@yandex.ru>
2021-12-11 12:46:23 +0300

    add ingridients and sostav to seeds

--- server/db/seeders/20211210152833-categories.js
+++ server/db/seeders/20211210152833-categories.js
@@ -5,31 +5,37 @@ module.exports = {
     
       await queryInterface.bulkInsert('Categories', [{
         name: 'Soups',
+        picture: "/img/categories/soup-cat.jpg",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         name: 'Drinks',
+        picture: "/img/categories/drinks-cat.jpg",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         name: 'Salats',
+        picture: "/img/categories/salats-cat.jpg",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         name: 'Garnish',
+        picture: "/img/categories/garnish-cat.jpg",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         name: 'Pizza',
+        picture: "/img/categories/pizza-cat.jpg",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         name: 'Deserts',
+        picture: "/img/categories/deaerts-cat.jpg",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
