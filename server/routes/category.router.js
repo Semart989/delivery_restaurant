1749822055bc4d2448be2f86d020801b2575ee58
Georgiy1437 <g.g.shishkin@gmail.com>
2021-12-11 19:14:43 +0300

    get dishes

--- server/routes/category.router.js
+++ server/routes/category.router.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
-const { getCategories } = require('../controllers/category.controller');
+const { getCategories, getCategoryDishes } = require('../controllers/category.controller');
 
 router.get('/', getCategories);
+
+router.get('/:catid', getCategoryDishes);
 
 module.exports = router;
