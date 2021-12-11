1d2c9bd591ad172b9404f33a3e8df82a6a736a12
Artem <a.semenov989@gmail.com>
2021-12-11 13:31:26 +0300

    pull last version

--- server/controllers/order.controller.js
+++ server/controllers/order.controller.js
@@ -1,16 +1,34 @@
-const axios = require('axios');
-const { Order, Dish } = require('../db/models');
+// const axios = require('axios');
+const { Order, Dish, Order_Dish } = require('../db/models');
+
+const order = {
+  user_id: 5,
+  dishes: [{
+    // order_id: 4,
+    dish_id: 10,
+    quantity: 2,
+  }, {
+    // order_id: 4,
+    dish_id: 12,
+    quantity: 1,
+  }],
+};
 
 const newOrder = async (req, res) => {
-  const { order } = req.body;
+  // const { order } = req.body;
 
   try {
     await Order.create({
-
+      user_id: order.user_id,
     });
 
-    await Dish.create({
+    const orderId = await Order.findAll({ where: { user_id: order.user_id } });
 
+    
+    await Order_Dish.create({
+      order_id: 123,
+      dish_id: 456,
+      quantity: 2,
     });
 
     res.status(200).json({ success: true });
--- server/db/seeders/20211210152833-categories.js
+++ server/db/seeders/20211210152833-categories.js
@@ -1,43 +1,39 @@
-'use strict';
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    
-      await queryInterface.bulkInsert('Categories', [{
-        name: 'Soups',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: 'Drinks',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: 'Salats',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: 'Garnish',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: 'Pizza',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: 'Deserts',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
+    await queryInterface.bulkInsert('Categories', [{
+      name: 'Soups',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      name: 'Drinks',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      name: 'Salats',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      name: 'Garnish',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      name: 'Pizza',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      name: 'Deserts',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
     ], {});
-
   },
 
   down: async (queryInterface, Sequelize) => {
-      await queryInterface.bulkDelete('Categories', null, {});
-  }
+    await queryInterface.bulkDelete('Categories', null, {});
+  },
 };
