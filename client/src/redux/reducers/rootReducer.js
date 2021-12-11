38893d11c4a03797f443ebdebd018d39afe6cac1
Fedor A <fodor2994@mail.ru>
2021-12-11 22:06:30 +0300

    fix rootReducer

--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import { userReducer } from './userReducer';
+import { cartReducer } from './cartReducer';
+import { ordersReducer } from './ordersReducer';
 
 const rootReducer = combineReducers({
   user: userReducer,
+  cart: cartReducer,
+  orders: ordersReducer,
 });
 
 export default rootReducer;
