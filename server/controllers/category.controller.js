77c4fa8f4e7b5352d404ec3507146d408ff43344
Georgiy Shishkin <87388438+Georgiy1437@users.noreply.github.com>
2021-12-11 18:24:17 +0300

    Update category.controller.js

--- server/controllers/category.controller.js
+++ server/controllers/category.controller.js
@@ -4,9 +4,6 @@ const { Category } = require('../db/models');
 const getCategories = async (req, res) => {
   try {
     const categories = await Category.findAll({});
-
-    console.log(categories);
-
     res.status(200).json({ categories });
   } catch (error) {
     res.status(404).json({ error: 'error' });
