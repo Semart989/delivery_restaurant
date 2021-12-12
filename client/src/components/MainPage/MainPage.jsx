dc36871e50ea94708c01b1da545d2f5806bba715
Sergey <sergey.s1991@yandex.ru>
2021-12-12 15:36:09 +0300

    front

--- client/src/components/MainPage/MainPage.jsx
+++ client/src/components/MainPage/MainPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ControlledCarousel from '../Carousel/Carousel'
 import DishCategory from '../DishCategory/DishCategory'
+import SFooter from '../SFooter/SFooter'
 
 function MainPage(props) {
   return (
     <div>
       <ControlledCarousel />
       <DishCategory />
+      <SFooter/>
     </div>
   );
 }
