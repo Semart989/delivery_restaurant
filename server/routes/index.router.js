a6b8a84423e10179c5130c5fd2836af6632d2d20
Alex Kos <aleksei-kosenko@mail.ru>
2021-12-13 18:48:21 +0300

    add_node_fetch

--- server/routes/index.router.js
+++ server/routes/index.router.js
@@ -4,11 +4,13 @@ const orderRouter = require('./order.router');
 const isAuthRouter = require('./isAuth.router');
 const categoryRouter = require('./category.router');
 const dishRouter = require('./dish.router');
+const testRouter = require('./test_ajax.router')
 
 router.use('/cart', orderRouter);
 router.use('/isauth', isAuthRouter);
 router.use('/categories', categoryRouter);
 // router.use('/', categoryRouter);
 router.use('/dishes', dishRouter);
+router.use('/test', testRouter);
 
 module.exports = router;
