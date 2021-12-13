243abd2d34eefbba3abed6866c921356bf570e57
Fedor A <fodor2994@mail.ru>
2021-12-13 20:34:17 +0300

    added saga for category render

--- client/src/redux/sagas/sagas.js
+++ client/src/redux/sagas/sagas.js
@@ -1,6 +1,6 @@
 import { call, put, takeEvery } from 'redux-saga/effects';
 import cartAT from '../actionTypes/cartAT';
-// import categoriesAT from '../actionTypes/categoriesAT';
+import categoriesAT from '../actionTypes/categoriesAT';
 
 // любая асинхронная функция, обычно используется для fetch()
 async function fetchData({ url, method, headers, body }) {
@@ -23,22 +23,20 @@ function* postFetchSendOrder(action) {
   }
 }
 
-// function* getFetchInitCategories(action) {
-//   try {
-//     const categories = yield call(fetchData, {
-//       url: 'http://localhost:4000/categories',
-//       method: 'GET',
-//       headers: { 'content-type': 'application/json' },
-//       // body: JSON.stringify(action.payload),
-//     });
-//     console.log('CATEGORIES', categories);
-//     yield put({ type: categoriesAT.INIT_CATEGORIES, payload: categories });
-//   } catch (err) {
-//     yield put({ type: categoriesAT.INIT_ERROR_CATEGORIES, payload: err })
-//   }
-// }
+function* getFetchInitCategories() {
+  try {
+    const categories = yield call(fetchData, {
+      url: '/categories',
+      method: 'GET',
+      headers: { 'content-type': 'application/json' },
+    });
+    yield put({ type: categoriesAT.INIT_CATEGORIES, payload: categories });
+  } catch (err) {
+    yield put({ type: categoriesAT.INIT_ERROR_CATEGORIES, payload: err })
+  }
+}
 
 export function* mySaga() {
   yield takeEvery(cartAT.POST_SEND_CART, postFetchSendOrder);
-  // yield takeEvery(categoriesAT.GET_CATEGORIES, getFetchInitCategories);
+  yield takeEvery(categoriesAT.GET_FETCH_CATEGORIES, getFetchInitCategories);
 }
