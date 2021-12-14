edd0851a8691c0879db23cdd3258a8833a963e9c
Georgiy1437 <g.g.shishkin@gmail.com>
2021-12-14 19:05:02 +0300

    search bar

--- client/src/components/MainPage/MainPage.jsx
+++ client/src/components/MainPage/MainPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ControlledCarousel from '../Carousel/Carousel'
 import CategoryList from '../CategoryList/CategoryList'
+import SearchAppBar from '../SearchAppBar/SearchAppBar';
 import SFooter from '../SFooter/SFooter'
 
 function MainPage(props) {
   return (
     <div>
+      <SearchAppBar/>
       <ControlledCarousel />
       <CategoryList />
       <SFooter/>
