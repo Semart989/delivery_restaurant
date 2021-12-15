b08be882fdabe0195134fa158a822d077c567551
Georgiy1437 <g.g.shishkin@gmail.com>
2021-12-15 09:31:47 +0300

    fixdishesitem

--- client/src/components/MainPage/MainPage.jsx
+++ client/src/components/MainPage/MainPage.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ControlledCarousel from '../Carousel/Carousel'
 import CategoryList from '../CategoryList/CategoryList'
-import SearchAppBar from '../SearchAppBar/SearchAppBar';
 import SFooter from '../SFooter/SFooter'
 
 function MainPage(props) {
   return (
     <div>
-      <SearchAppBar/>
       <ControlledCarousel />
       <CategoryList />
       <SFooter/>
